4215cbe0f606a6c010b94384c79221d8bc678a74
Duc Thang <thangpdth2009031@fpt.edu.vn>
2020-12-10 10:36:51 +0700

    L11

--- js/create.js
+++ js/create.js
@@ -31,6 +31,14 @@ document.addEventListener('DOMContentLoaded', function () {
                     alert("Thêm bài hát không thành công!");
                 }
             }
+            axios.interceptors.response.use(response => {
+                return response;
+            }, error => {
+                if (error.response.status === 401) {
+                    //place your reentry code
+                }
+                return error;
+            });
             xhr.send(registerDataJson);
         }
     }
